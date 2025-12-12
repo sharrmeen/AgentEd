f7bb26c5137d6e697474cf5966b6ebf61ab9921d
MandarMawale-20 <141636855+MandarMawale-20@users.noreply.github.com>
2025-12-12 19:17:02 +0530

    CUDA setup for gpu

--- testgpu.py
+++ testgpu.py
@@ -1,3 +1,7 @@
+#ONLY FOR NVIDIA GPU
+# You may need to adjust the CUDA version based on what is installed on your PC.  
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118  #Change thenversion here eg cu130
+
 import torch
 print(f"CUDA available: {torch.cuda.is_available()}")
 print(f"GPU Name: {torch.cuda.get_device_name(0)}" if torch.cuda.is_available() else "GPU not found.")
